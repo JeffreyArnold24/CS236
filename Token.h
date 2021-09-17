c78b59b9234a9c15aa0ea3e93826a45ba4fc87ce
root <root@DESKTOP-KMVSAKP.localdomain>
2021-09-16 22:12:15 -0600

    First Commit

---
+++ Token.h
@@ -0,0 +1,25 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <string>
+using namespace std;
+
+class Token
+{
+private:
+    string tokenName;
+    int lineNumber;
+    string type;
+
+public:
+    void setType(string);
+    void setValue(string);
+    void setLine(int);
+
+
+    string getType();
+    string getValue();
+    string getLine();
+};
+
+#endif // TOKEN_H
+
